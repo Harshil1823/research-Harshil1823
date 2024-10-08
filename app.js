4823d6511674a32199b8e7d5e1ce4ae0f312b96f
Harshil Shah <harshilshah@Harshils-MBP-2.attlocal.net>
2024-10-08 12:05:59 -0400

    added fake data to mongodb with this function createfakeComments

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+//acquire the model from mongoose
+const { Comment, createFakeComments } = require('./models/comments')
 
 async function main() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/commentsApp');
         console.log('Mongoose connection is open');
+        createFakeComments();
     }
     catch (err) {
         console.log('connection failed', err);
     }
 }
 
-
+main();
 
 app.get('/', (req, res) => {
     res.send('hello world')
