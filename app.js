69f7ab6ebcb4c81cf9f79fce84d25c8cf81f91b6
Harshil Shah <harshilshah@Harshils-MBP-2.attlocal.net>
2024-10-08 11:41:58 -0400

    mongoose connected with mongodb

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/commentsApp');
+        console.log('Mongoose connection is open');
+    }
+    catch (err) {
+        console.log('connection failed', err);
+    }
+}
+
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
